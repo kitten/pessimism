d288cff9d5b0053bb8d810a6480cb05c93ce05d7
Phil Pluckthun <phil@kitten.sh>
2019-08-14 22:13:15 +0100

    Add TypeScript typings

---
+++ src/pessimism.d.ts
@@ -0,0 +1,13 @@
+export class Map<T> {}
+
+export const make: <T>() => Map<T>;
+export const get: <T>(map: Map<T>, key: string) => undefined | T;
+export const remove: <T>(map: Map<T>, key: string) => Map<T>;
+export const set: <T>(map: Map<T>, key: string, value: T) => Map<T>;
+export const setOptimistic: <T>(
+  map: Map<T>,
+  key: string,
+  value: T,
+  optimisticKey: number
+) => Map<T>;
+export const clearOptimistic: <T>(map: Map<T>, optimisticKey: number) => Map<T>;
