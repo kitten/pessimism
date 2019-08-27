e391b0b9e70b84d26ce21dee7e0d40b46676ea25
Phil Pluckthun <phil@kitten.sh>
2019-08-27 16:19:12 +0100

    Minify all bundles

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import prettier from 'rollup-plugin-prettier';
 
 const plugins = [
   nodeResolve({
@@ -26,38 +25,20 @@ const plugins = [
     extensions: [...DEFAULT_EXTENSIONS, 'ts', 'tsx'],
     exclude: 'node_modules/**',
     presets: [],
-    plugins: [['babel-plugin-closure-elimination', {}], ['babel-plugin-tailcall-optimization', {}]]
+    plugins: [['babel-plugin-closure-elimination', {}]]
   }),
 
   terser({
     warnings: true,
     ecma: 5,
-    keep_fnames: true,
     ie8: false,
-    compress: {
-      pure_getters: true,
-      toplevel: true,
-      booleans_as_integers: false,
-      keep_fnames: true,
-      keep_fargs: true,
-      if_return: false,
-      ie8: false,
-      sequences: false,
-      loops: false,
-      conditionals: false,
-      join_vars: false
-    },
-    mangle: false,
+    mangle: true,
+    toplevel: true,
     output: {
       beautify: true,
       braces: true,
       indent_level: 2
     }
-  }),
-  prettier({
-    parser: 'babel',
-    singleQuote: true,
-    printWidth: 100
   })
 ];
 
