e98a4f74018e9ada62adb7dd5f0306af7c3f5c8a
Phil Pluckthun <phil@kitten.sh>
2019-08-27 18:25:26 +0100

    Fix TypeScript typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,6 @@ export class Map<T> {}
 export const make: <T>() => Map<T>;
 export const asMutable: <T>(map: Map<T>) => Map<T>;
 export const asImmutable: <T>(map: Map<T>) => Map<T>;
-export const make: <T>() => Map<T>;
 export const get: <T>(map: Map<T>, key: string) => undefined | T;
 export const remove: <T>(map: Map<T>, key: string) => Map<T>;
 export const set: <T>(map: Map<T>, key: string, value: T) => Map<T>;
